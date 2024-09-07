36c0222fb43963634d8936ddc95b17a1684728d4
Hamza Jalal <128197662+NotHammy@users.noreply.github.com>
2024-09-07 22:50:03 +1000

    Update School_data_exploration.sql

--- Data Bees Project/School_data_exploration.sql
+++ Data Bees Project/School_data_exploration.sql
@@ -37,7 +37,7 @@ PercentageChangeData as (
 	round(cast((Total_Enrolments - lag(Total_Enrolments) over (partition by School_Name order by Calendar_Year)) * 100.0 as float) / cast(lag(Total_Enrolments) over (partition by School_Name order by Calendar_Year) as float), 2) as Percentage_Change
 	from EnrolmentData
 )
-select school_name, cast(avg(Percentage_Change) as decimal(10, 2)) as Avg_Percentage_Change
+select top 10 school_name, cast(avg(Percentage_Change) as decimal(10, 2)) as Avg_Percentage_Change
 from PercentageChangeData
 group by School_name
 order by Avg_Percentage_Change desc;
@@ -64,10 +64,9 @@ PercentageChangeData as (
 select Suburb, cast(avg(Percentage_Change) as decimal(10, 2)) as Avg_Percentage_Change
 from PercentageChangeData
 group by Suburb
-order by Avg_Percentage_Change desc;
+order by Avg_Percentage_Change desc
 
 
-|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
 
 -- This query proves that there is no data for Dayton Primary and Emmaus Catholic since they are new schools
 -- For this reason the growth factor is exponentially high for the suburb Dayton and so it must be excluded from the 
@@ -156,14 +155,150 @@ order by ICSEA desc
 |-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
 
 
--- find the richest schools with the most academically ambitious students, most athletes, and most travelling students. 
--- Top sea quarter % represents the number of students at school which are from the highest socio-educational advantage.
--- So schools with higher Top sea Quarter % will tend to have more students in the top 25% of Australia in terms of social educational advantage.
--- Advertising to schools with higher Top_Sea_Quarter % value will yield a greater result when advertising.
-select school_name, Suburb, Top_SEA_Quarter 
-from School
-where Top_SEA_Quarter is not null
-order by Top_SEA_Quarter desc
+-- Rural and regional suburbs with the highest ICSEA scores and the most enrolment numbers 
+-- Group based of a weighted average score to get most ideal rural suburbs to advertise to
+-- This weighted average score is based on avg(ICSEA_Percentile) and avg(total_enrolments) of a suburb
+
+-- ICSEA Percentile and total enrolments per suburb since 2020
+;with EnrolmentData as (
+	select Calendar_Year, Suburb, ICSEA_Percentile,  sum(Total_Enrolments) as Total_Enrolments
+	from School
+	where Calendar_Year >= '2020' 
+	and Geolocation in ('Outer Regional', 'Remote', 'Very Remote')
+	and Total_Enrolments is not null		-- Exclude suburbs with no enrollment data
+	and Total_Enrolments > 100
+	group by Calendar_Year, Suburb, ICSEA_Percentile
+),
+-- Normalise the data
+NormalisedData as (
+	select Suburb, avg(ICSEA_Percentile) as avg_ICSEA_percentile, avg(Total_Enrolments) as avg_total_enrolments,
+		max(avg(ICSEA_Percentile)) over () as max_ICSEA_percentile,
+		min(avg(ICSEA_Percentile)) over () as min_ICSEA_percentile,
+		max(avg(Total_Enrolments)) over () as max_total_enrolments,
+		min(avg(Total_Enrolments)) over () as min_total_enrolments
+	from EnrolmentData
+	group by Suburb
+)
+-- list of suburbs based on weighted score
+-- NOTE: See formula in Methodology section in Report
+select top 10 Suburb, avg_ICSEA_percentile, avg_total_enrolments, 
+	  round(
+        (
+            (
+                (cast(avg_ICSEA_percentile as float) - cast(min_ICSEA_percentile as float)) / 
+                (cast(max_ICSEA_percentile as float) - cast(min_ICSEA_percentile as float))*0.67
+            ) + 
+            (
+                (cast(avg_total_enrolments as float) - cast(min_total_enrolments as float)) / 
+                (cast(max_total_enrolments as float) - cast(min_total_enrolments as float))*0.33
+            )
+        ),
+        3
+    ) as weighted_score
+from NormalisedData
+order by weighted_score desc;
+
+
+
+|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+
+
+
+-- Above we found the best rural and regional suburbs for advertisement.
+-- Query below finds the best rural and regional schools instead.
+
+
+;with EnrolmentData as (
+	select Calendar_Year, Suburb, School_Name, ICSEA_Percentile,  sum(Total_Enrolments) as Total_Enrolments
+	from School
+	where Calendar_Year >= '2020' 
+	and Geolocation in ('Outer Regional', 'Remote', 'Very Remote')
+	and Total_Enrolments is not null		-- Exclude suburbs with no enrollment data
+	group by Calendar_Year, School_Name, Suburb,  ICSEA_Percentile
+),
+-- Normalise the data
+NormalisedData as (
+	select School_Name, Suburb, avg(ICSEA_Percentile) as avg_ICSEA_percentile, avg(Total_Enrolments) as avg_total_enrolments,
+		max(avg(ICSEA_Percentile)) over () as max_ICSEA_percentile,
+		min(avg(ICSEA_Percentile)) over () as min_ICSEA_percentile,
+		max(avg(Total_Enrolments)) over () as max_total_enrolments,
+		min(avg(Total_Enrolments)) over () as min_total_enrolments
+	from EnrolmentData
+	group by School_Name, Suburb
+)
+select top 10 School_Name, Suburb, avg_ICSEA_percentile, avg_total_enrolments, 
+	  round(
+        (
+            (
+                (cast(avg_ICSEA_percentile as float) - cast(min_ICSEA_percentile as float)) / 
+                (cast(max_ICSEA_percentile as float) - cast(min_ICSEA_percentile as float))*0.67
+            ) + 
+            (
+                (cast(avg_total_enrolments as float) - cast(min_total_enrolments as float)) / 
+                (cast(max_total_enrolments as float) - cast(min_total_enrolments as float))*0.33
+            )
+        ),
+        3
+    ) as weighted_score
+from NormalisedData
+order by weighted_score desc;
+
+
+
+
+
+|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+
+
+-- Special needs schools with high ICSEA percentiles and high enrollment counts
+
+
+;with EnrolmentData as (
+	select Calendar_Year, Suburb, School_Name, ICSEA_Percentile,  sum(Total_Enrolments) as Total_Enrolments, School_Type
+	from School
+	where Calendar_Year >= '2020' 
+	and Total_Enrolments is not null		-- Exclude suburbs with no enrollment data
+	and School_Type = 'Special'
+	group by Calendar_Year, School_Name, Suburb,  ICSEA_Percentile, School_Type
+),
+-- Normalise the data
+NormalisedData as (
+	select School_Name, Suburb, avg(ICSEA_Percentile) as avg_ICSEA_percentile, avg(Total_Enrolments) as avg_total_enrolments, School_Type,
+		max(avg(ICSEA_Percentile)) over () as max_ICSEA_percentile,
+		min(avg(ICSEA_Percentile)) over () as min_ICSEA_percentile,
+		max(avg(Total_Enrolments)) over () as max_total_enrolments,
+		min(avg(Total_Enrolments)) over () as min_total_enrolments
+	from EnrolmentData
+	group by School_Name, Suburb, School_Type
+)
+select School_Name, Suburb, School_Type, avg_ICSEA_percentile, avg_total_enrolments, 
+	  round(
+        (
+            (
+                (cast(avg_ICSEA_percentile as float) - cast(min_ICSEA_percentile as float)) / 
+                (cast(max_ICSEA_percentile as float) - cast(min_ICSEA_percentile as float))*0.67
+            ) + 
+            (
+                (cast(avg_total_enrolments as float) - cast(min_total_enrolments as float)) / 
+                (cast(max_total_enrolments as float) - cast(min_total_enrolments as float))*0.33
+            )
+        ),
+        3
+    ) as weighted_score
+from NormalisedData
+order by weighted_score desc;
+
+
+
+|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+
+
+
+
+
+
+
+
 
 
 
