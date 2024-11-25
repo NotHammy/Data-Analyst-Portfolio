0535f4e256060bdbab58d5f304770560c8d37ae7
Hamza Jalal <hamzajalal23@gmail.com>
2024-11-25 20:01:30 +1100

    created map visualisation

--- NYC Parking Violations/SQLQuery1.sql
+++ NYC Parking Violations/SQLQuery1.sql
@@ -34,18 +34,48 @@ SELECT TOP 100 * FROM nyc;
 
 
 
--- Exploratory Data Analysis
+------------------------------------------------------------------------ Exploratory Data Analysis ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Which parking violation was most commonly committed by vehicles in NYC that were registered in various states?
-WITH RankedViolations AS (
-    SELECT Registration_State AS State, Violation_Description AS Violation, COUNT(Violation_Description) AS Count, ROW_NUMBER() OVER (PARTITION BY Registration_State ORDER BY COUNT(Violation_Description) DESC) AS Rank
-    FROM nyc 
-    GROUP BY Registration_State, Violation_Description
+-- Include percentage distribution of violations.
+WITH TotalViolations AS (
+    SELECT SUM(COUNT(*)) OVER () AS TotalCount
+    FROM nyc
+),
+StateViolations AS (
+    SELECT Registration_State AS State, COUNT(*) AS Count
+    FROM nyc
+    GROUP BY Registration_State
+),
+RankedViolations AS (
+    SELECT State, Count, ROUND((CAST(Count AS FLOAT) / TotalCount) * 100, 2) AS Percentage, ROW_NUMBER() OVER (PARTITION BY State ORDER BY Count DESC) AS Rank
+    FROM StateViolations, TotalViolations
 )
-SELECT State, Violation, Count
+SELECT TOP 30 State, Count, Percentage
 FROM RankedViolations
-WHERE Rank = 1
-ORDER BY COUNT DESC;
+WHERE State <> 'NY'  -- Excluding New York from the final results
+ORDER BY Percentage DESC;
 
 
--- testing if changed made on mssms sync with github
+
+
+
+
+-- Total number of violations committed in NYC in the current Fiscal Year
+SELECT COUNT(DISTINCT Summons_Number) AS Total_Violations
+FROM nyc; 
+
+
+
+-- Most Commonly Committed Parking Violations and their distributions
+SELECT TOP 10 Violation, Count, ROUND((CAST(Count AS FLOAT) / TotalViolations) * 100, 2) AS Percentage
+FROM (
+	SELECT Violation_Description AS Violation, COUNT(Violation_Description) AS Count
+    FROM nyc
+    GROUP BY Violation_Description
+) AS Violations
+CROSS JOIN (
+	SELECT COUNT(Violation_Description) AS TotalViolations
+    FROM nyc
+) AS Total
+ORDER BY Count DESC;
